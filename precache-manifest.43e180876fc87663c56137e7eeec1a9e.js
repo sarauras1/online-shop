bf0fca0970802d6ff60221c3e6c04e21c1de8538
sarauras1 <41049600+sarauras1@users.noreply.github.com>
2020-06-24 15:15:24 +0100

    Updates

---
+++ precache-manifest.43e180876fc87663c56137e7eeec1a9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9766d0cccd89976c0ecd09ce692001f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21547857d722925194f7",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b3c5ee609702018a1d64",
+    "url": "/static/css/main.a07ec5c7.chunk.css"
+  },
+  {
+    "revision": "21547857d722925194f7",
+    "url": "/static/js/2.2d478d9d.chunk.js"
+  },
+  {
+    "revision": "b3c5ee609702018a1d64",
+    "url": "/static/js/main.f129eae2.chunk.js"
+  },
+  {
+    "revision": "6ebc0bf0fcdfacc043db",
+    "url": "/static/js/runtime~main.fc457e6f.js"
+  },
+  {
+    "revision": "c29aefb63398c9e8754972c39ac579d4",
+    "url": "/static/media/logo_LAKSHMI-1.c29aefb6.jpg"
+  }
+]);
